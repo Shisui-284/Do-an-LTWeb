d880023693891e01cda685fb59613a993a3897ca
Dang Phuong Nam <hao@example.com>
2026-06-20 13:33:37 +0700

    fix: restore api url with https proxy

--- frontend/hotel-booking-web/src/main.jsx
+++ frontend/hotel-booking-web/src/main.jsx
@@ -4,11 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
 
-// Gắn sẵn "chìa khóa" vào mọi yêu cầu axios gửi đi
-axios.defaults.auth = {
-  username: '11316812',
-  password: '60-dayfreetrial'
-};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
